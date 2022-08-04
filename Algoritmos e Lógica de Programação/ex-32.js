d4566255398ed8ca33e7f11a55031a903e3fdbae
eliabearaujo <eliabe_trajano@hotmail.com>
2022-08-04 01:21:24 -0300

    Novos Exercicios

--- Algoritmos e Lógica de Programação/ex-32.js
+++ Algoritmos e Lógica de Programação/ex-32.js
@@ -1,1 +1,10 @@
 // Crie uma função que recebe um array de números e retorna o segundo maior número presente nesse array.
+
+function segundoMaior(numeros) {
+  const numerosOrdenados = numeros.sort((a, b) => {
+    return b - a;
+  });
+  return numerosOrdenados[1];
+}
+
+console.log(segundoMaior([12, 16, 1, 5]));
