31b7814fad0a26d74c68fc629af9fd4a2289a948
eliabearaujo <eliabe_trajano@hotmail.com>
2022-08-18 02:04:15 -0300

    Curso de JavaScript - Origamid

---
+++ JavaScript - Origamid/1 - JavaScript para Iniciantes/1 - Variaveis.js
@@ -0,0 +1,22 @@
+// var nome = "André";
+// var idade = 28;
+// var possuiFaculdade = true;
+
+// var nome = "André",
+//   idade = 28,
+//   possuiFaculdade = true;
+
+// console.log(nome);
+
+let nome = "Eliabe";
+let idade = "26";
+let comidaFavorita;
+comidaFavorita = "Pizza";
+let nomeDoPai,
+  nomeDaMae,
+  idadeDoCachorro,
+  possuiCarteiraDeMotorista,
+  valorNaCarteira;
+console.log(nome);
+console.log(idade);
+console.log(comidaFavorita);
---
+++ JavaScript - Origamid/1 - JavaScript para Iniciantes/2 - Tipos de Dados.js
@@ -0,0 +1,40 @@
+// var nome = "André"; // String
+// var idade = 28; // Number
+// var possuiFaculdade = true; // Boolean
+// var time; // Undefined
+// var comida = null; // Null
+// var simbolo = Symbol(); // Symbol
+// var novoObjeto = {}; // Object
+
+// console.log(typeof nome); // retorna string
+
+// var nome = "André";
+// var sobrenome = "Rafael";
+// var nomeCompleto = nome + " " + sobrenome;
+
+// console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = "Romário fez " + gols + " gols";
+// console.log(frase);
+// console.log(typeof frase);
+
+// Declare uma variável contendo uma string
+let frase = "Salve";
+// Declare uma variável contendo um número dentro de uma string
+let idadeString = "26";
+// Declare uma variável com a sua idade
+let idade = 26;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let nome = "Eliabe";
+let sobrenome = "Araujo";
+let nomeCompleto = nome + " " + sobrenome;
+let nomeCompleto2 = `${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+console.log(nomeCompleto2);
+// Coloque a seguinte frase em uma variável: It's time
+let frase2 = "It's time";
+let frase2 = "It's time";
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
