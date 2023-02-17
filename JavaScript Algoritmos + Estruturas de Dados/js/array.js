17dc560e7aa972aa7aae1bb4a0802463ef04689a
eliabearaujo <eliabe_trajano@hotmail.com>
2023-02-16 23:51:31 -0300

    Aulas sobre Arrays finalizada

--- JavaScript Algoritmos + Estruturas de Dados/js/array.js
+++ JavaScript Algoritmos + Estruturas de Dados/js/array.js
@@ -71,3 +71,36 @@ numbers.shift();
 numbers.shift();
 numbers.shift();
 numbers.shift();
+
+// -------------------------------------------------------------------
+// let avgTempWeek = [];
+// let avgTempWeek1 = [33, 25.2, 19, 27, 23.4, 41.6, 25];
+// let avgTempWeek2 = [41, 29, 33, 21.2, 19.5, 17, 33.8];
+
+// avgTempWeek[0] = avgTempWeek1;
+// avgTempWeek[1] = avgTempWeek2;
+
+// for (let i = 0; i < avgTempWeek.length; i++) {
+//   for (let j = 0; j < avgTempWeek[i].length; j++) {
+//     console.log(avgTempWeek[i][j]);
+//   }
+// }
+
+// -------------------------------------------------------------------
+let month = [];
+let firstWeeks = [];
+let lastWeeks = [];
+let avgTempWeek1 = [33, 25.2, 19, 27, 23.4, 41.6, 25];
+let avgTempWeek2 = [41, 29, 33, 21.2, 19.5, 17, 33.8];
+let avgTempWeek3 = [13, 45.7, 18, 7, 13.4, 21.5, 15];
+let avgTempWeek4 = [51, 19, 23, 26.2, 9.5, 14.4, 31.8];
+
+firstWeeks = [avgTempWeek1, avgTempWeek2];
+lastWeeks = [avgTempWeek3, avgTempWeek4];
+month = [firstWeeks, lastWeeks];
+
+for (let i = 0; i < avgTempWeek.length; i++) {
+  for (let j = 0; j < avgTempWeek[i].length; j++) {
+    console.log(avgTempWeek[i][j]);
+  }
+}
